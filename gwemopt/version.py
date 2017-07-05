82f2cc33d04a85b95e76a95ef6dedd9b5f05508d
Shaon Ghosh <ghosh4@uwm.edu>
2017-07-04 20:43:20 -0500

    versions.py pushed

--- gwemopt/version.py
+++ gwemopt/version.py
@@ -5,10 +5,10 @@
 """
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
-__version__ = 'c5bc1d6b4c83d02e54bf709e0855c1a457268090'
-__date__ = '2017-07-04 19:17:54 +0000'
+__version__ = 'c8f8d3ac23ecadfb11e58ee9026f1d4cfa89aaef'
+__date__ = '2017-07-04 19:23:40 +0000'
 
-git_id = 'c5bc1d6b4c83d02e54bf709e0855c1a457268090'
+git_id = 'c8f8d3ac23ecadfb11e58ee9026f1d4cfa89aaef'
 git_branch = 'master'
 git_tag = None
 git_author = 'Michael Coughlin <michael.coughlin@ligo.org>'
