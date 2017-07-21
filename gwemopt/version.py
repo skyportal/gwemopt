aa25a87b01b1181b5fd0297e8fdcf500f1a5dd47
Duo Tao <taod@carleton.edu>
2017-07-21 14:00:33 -0500

    require --do3D for --doWAW

--- gwemopt/version.py
+++ gwemopt/version.py
@@ -5,12 +5,12 @@
 """
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
-__version__ = '42fe96e66d3ad62ac0bac638d4edfccb2de97a6b'
-__date__ = '2017-07-19 19:08:14 +0000'
+__version__ = '16176a0b0e03a109bdc69c32c92c1c67e692df29'
+__date__ = '2017-07-20 20:27:06 +0000'
 
-git_id = '42fe96e66d3ad62ac0bac638d4edfccb2de97a6b'
+git_id = '16176a0b0e03a109bdc69c32c92c1c67e692df29'
 git_branch = 'master'
 git_tag = None
 git_author = 'Michael Coughlin <michael.coughlin@ligo.org>'
 git_committer = 'Michael Coughlin <michael.coughlin@ligo.org>'
-git_status = None
+git_status = 'UNCLEAN: Modified working tree'
