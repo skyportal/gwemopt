2b8a31584e29ffdcce5133e51d8ad75d0d37778d
Duo Tao <taod@carleton.edu>
2017-07-27 21:27:05 -0500

    example code in doc, doefficiency requirements remove, require doTiles for doSchedule

--- gwemopt/version.py
+++ gwemopt/version.py
@@ -5,12 +5,12 @@
 """
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
-__version__ = '46f48f61c4db793d613f20988ab6d73d4bcd61d6'
-__date__ = '2017-07-26 16:17:06 +0000'
+__version__ = '3d053f88128f1005dc3507e31664c67b31f54c4c'
+__date__ = '2017-07-26 19:34:02 +0000'
 
-git_id = '46f48f61c4db793d613f20988ab6d73d4bcd61d6'
+git_id = '3d053f88128f1005dc3507e31664c67b31f54c4c'
 git_branch = 'master'
 git_tag = None
-git_author = 'Michael Coughlin <michael.coughlin@ligo.org>'
-git_committer = 'Michael Coughlin <michael.coughlin@ligo.org>'
+git_author = 'Michael Coughlin <coughlin@physics.harvard.edu>'
+git_committer = 'Michael Coughlin <coughlin@physics.harvard.edu>'
 git_status = None
