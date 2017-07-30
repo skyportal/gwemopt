a09b684462433805c40ae778e19a3c5a8c680d87
Duo Tao <taod@carleton.edu>
2017-07-30 00:50:28 -0500

    put version.py back

---
+++ gwemopt/version.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Duncan Macleod (2013)
+
+"""Versioning record for CIS
+"""
+
+__author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
+__version__ = 'b8df0cbe12b2b8085943461decec5ac525520edc'
+__date__ = '2017-07-29 20:12:39 +0000'
+
+git_id = 'b8df0cbe12b2b8085943461decec5ac525520edc'
+git_branch = 'master'
+git_tag = None
+git_author = 'Duo Tao <taod@carleton.edu>'
+git_committer = 'Duo Tao <taod@carleton.edu>'
+git_status = None
