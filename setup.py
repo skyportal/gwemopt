dc45e27639d83cb48f70ccca9bee7d1ba89dea68
Michael Coughlin <coughlin@physics.harvard.edu>
2018-02-09 15:29:18 -0800

    Remove setup_utils.py

--- setup.py
+++ setup.py
@@ -25,21 +25,29 @@
 
 from __future__ import print_function
 
-import sys
+import os, sys
 from distutils.version import LooseVersion
 
 from setuptools import (setup, find_packages,
                         __version__ as setuptools_version)
 
-import versioneer
-from setup_utils import (CMDCLASS, get_setup_requires, get_scripts)
+def get_scripts(scripts_dir='bin'):
+    """Get relative file paths for all files under the ``scripts_dir``
+    """ 
+    scripts = []
+    for (dirname, _, filenames) in os.walk(scripts_dir):
+        scripts.extend([os.path.join(dirname, fn) for fn in filenames])
+    return scripts
 
+import versioneer
+#from setup_utils import (CMDCLASS, get_setup_requires, get_scripts)
 __version__ = versioneer.get_version()
+CMDCLASS=versioneer.get_cmdclass()
 
 # -- dependencies -------------------------------------------------------------
 
 # build dependencies
-setup_requires = get_setup_requires()
+#setup_requires = get_setup_requires()
 
 # package dependencies
 install_requires = [
@@ -82,7 +90,6 @@ setup(
 
     # dependencies
     cmdclass=CMDCLASS,
-    setup_requires=setup_requires,
     install_requires=install_requires,
     tests_require=tests_require,
 
