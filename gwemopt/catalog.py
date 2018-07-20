a1f57ffaf0288faaf63a10d8c53e044fa65d9f2f
Michael Coughlin <coughlin@physics.harvard.edu>
2018-07-20 16:15:28 -0700

    Add log10 back in

--- gwemopt/catalog.py
+++ gwemopt/catalog.py
@@ -62,6 +62,7 @@ def get_catalog(params, map_struct):
         L_nu =  const * 10.**((mag + MAB0)/(-2.5))
 
     L_nu =  const * 10.**((mag + MAB0)/(-2.5))
+    L_nu = np.log10(L_nu)
     L_nu = L_nu**params["catalog_n"]
     Slum = L_nu / np.sum(L_nu)
     mlim, M_KNmin, M_KNmax = 22, -17, -12
