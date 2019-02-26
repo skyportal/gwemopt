04dc182a2cf7edfff80a9c12f83d92d9fa6949dd
Michael Coughlin <coughlin@physics.harvard.edu>
2019-02-25 17:51:45 -0800

    Add tilesType == galaxy option

--- gwemopt/catalog.py
+++ gwemopt/catalog.py
@@ -1,6 +1,6 @@
 
 import os, sys, copy
-
+import h5py
 import numpy as np
 import healpy as hp
 
@@ -20,7 +20,7 @@ import astropy.constants as c
 def get_catalog(params, map_struct):
 
     if not os.path.isdir(params["catalogDir"]): os.makedirs(params["catalogDir"])
-    catalogFile = os.path.join(params["catalogDir"],"%s.csv"%params["galaxy_catalog"])
+    catalogFile = os.path.join(params["catalogDir"],"%s.hdf5"%params["galaxy_catalog"])
 
     """AB Magnitude zero point."""
     MAB0 = -2.5 * np.log10(3631.e-23)
@@ -30,41 +30,104 @@ def get_catalog(params, map_struct):
     if params["galaxy_catalog"] == "2MRS":
         if not os.path.isfile(catalogFile):
             cat, = Vizier.get_catalogs('J/ApJS/199/26/table3')
-            ascii.write(cat,catalogFile,format='ecsv')
+
+            ras, decs = cat["RAJ2000"], cat["DEJ2000"]
+            cz = cat["cz"]
+            magk = cat["Ktmag"]
+            z = (u.Quantity(cat['cz']) / c.c).to(u.dimensionless_unscaled)
+
+            completeness = 0.5
+            alpha = -1.0
+            MK_star = -23.55
+            MK_max = MK_star + 2.5 * np.log10(gammaincinv(alpha + 2, completeness))
+            MK = magk - cosmo.distmod(z)
+            idx = (z > 0) & (MK < MK_max)
+        
+            ra, dec = ra[idx], dec[idx]
+            z = z[idx]
+            magk = magk[idx]            
+
+            with h5py.File(catalogFile, 'w') as f:
+                f.create_dataset('ra', data=ras)
+                f.create_dataset('dec', data=decs)
+                f.create_dataset('z', data=z)
+                f.create_dataset('magk', data=magk)
+
         else:
-            cat = ascii.read(catalogFile,format='ecsv')
+            with h5py.File(catalogFile, 'r') as f:
+                ra, dec = f['ra'][:], f['dec'][:]
+                z = f['z'][:]
+                magk = f['magk'][:]
 
-        completeness = 0.5
-        alpha = -1.0
-        MK_star = -23.55
-        MK_max = MK_star + 2.5 * np.log10(gammaincinv(alpha + 2, completeness))
-
-        z = (u.Quantity(cat['cz']) / c.c).to(u.dimensionless_unscaled)
-        MK = cat['Ktmag'] - cosmo.distmod(z)
-        keep = (z > 0) & (MK < MK_max)
-
-        cat = cat[keep]
-        z = z[keep]
         r = cosmo.luminosity_distance(z).to('Mpc').value
-
-        mag = cat['Ktmag']
-        L_nu =  const * 10.**((mag + MAB0)/(-2.5))
+        mag = magk * 1.0
  
     elif params["galaxy_catalog"] == "GLADE":
         if not os.path.isfile(catalogFile):
             cat, = Vizier.get_catalogs('VII/281/glade2')
-            ascii.write(cat,catalogFile,format='ecsv')
-        else:
-            cat = ascii.read(catalogFile,format='ecsv')
-        r = cat["Dist"]
 
-        mag = cat['Bmag']
-        L_nu =  const * 10.**((mag + MAB0)/(-2.5))
+            ra, dec = cat["RAJ2000"], cat["DEJ2000"]
+            distmpc = cat["Dist"]
+            magb, magk = cat["Bmag"], cat["Kmag"]
+
+            idx = np.where(distmpc >= 0)[0]
+            ra, dec = ra[idx], dec[idx]
+            distmpc = distmpc[idx]
+            magb, magk = magb[idx], magk[idx]
+
+            with h5py.File(catalogFile, 'w') as f:
+                f.create_dataset('ra', data=ra)
+                f.create_dataset('dec', data=dec)
+                f.create_dataset('distmpc', data=distmpc)
+                f.create_dataset('magb', data=magb)
+                f.create_dataset('magk', data=magk)
+        else:
+            with h5py.File(catalogFile, 'r') as f:
+                ra, dec = f['ra'][:], f['dec'][:]
+                distmpc = f['distmpc'][:]
+                magb, magk = f['magb'][:], f['magk'][:]
+
+        idx = np.where(~np.isnan(magb))[0]
+        ra, dec, distmpc, magb, magk = ra[idx], dec[idx], distmpc[idx], magb[idx], magk[idx]
+
+        r = distmpc * 1.0
+        mag = magb * 1.0
+
+    elif params["galaxy_catalog"] == "CLU":
+        if not os.path.isfile(catalogFile):
+            print("Please add %s."%catalogFile)
+            exit(0)
+
+        with h5py.File(catalogFile, 'r') as f:
+            name = f['name'][:]
+            ra, dec = f['ra'][:], f['dec'][:]
+            sfr_fuv, mstar = f['sfr_fuv'][:], f['mstar'][:]
+            distmpc, magb = f['distmpc'][:], f['magb'][:]
+            a, b2a, pa = f['a'][:], f['b2a'][:], f['pa'][:]
+            btc = f['btc'][:]
+
+        idx = np.where(distmpc >= 0)[0]
+        ra, dec = ra[idx], dec[idx]
+        sfr_fuv, mstar = sfr_fuv[idx], mstar[idx]
+        distmpc, magb = distmpc[idx], magb[idx]
+        a, b2a, pa = a[idx], b2a[idx], pa[idx]
+        btc = btc[idx]
+
+        idx = np.where(~np.isnan(magb))[0]
+        ra, dec = ra[idx], dec[idx]
+        sfr_fuv, mstar = sfr_fuv[idx], mstar[idx]
+        distmpc, magb = distmpc[idx], magb[idx]
+        a, b2a, pa = a[idx], b2a[idx], pa[idx]
+        btc = btc[idx]
+
+        r = distmpc * 1.0
+        mag = magb * 1.0
 
     L_nu =  const * 10.**((mag + MAB0)/(-2.5))
     L_nu = np.log10(L_nu)
     L_nu = L_nu**params["catalog_n"]
     Slum = L_nu / np.sum(L_nu)
+
     mlim, M_KNmin, M_KNmax = 22, -17, -12
     L_KNmin, L_KNmax = const * 10.**((M_KNmin + MAB0)/(-2.5)), const * 10.**((M_KNmax + MAB0)/(-2.5))
 
@@ -73,9 +136,9 @@ def get_catalog(params, map_struct):
     Sdet[Sdet<0.01] = 0.01
     Sdet[Sdet>1.0] = 1.0
 
-    theta = 0.5 * np.pi - cat['DEJ2000'].to('rad').value
-    phi = cat['RAJ2000'].to('rad').value
-    ipix = hp.ang2pix(map_struct["nside"], cat['RAJ2000'].to('deg').value, cat['DEJ2000'].to('deg').value, lonlat=True)
+    theta = 0.5 * np.pi - dec * 2 * np.pi /360.0
+    phi = ra * 2 * np.pi /360.0
+    ipix = hp.ang2pix(map_struct["nside"], ra, dec, lonlat=True)
 
     if "distnorm" in map_struct:
         Sloc = map_struct["prob"][ipix] * (map_struct["distnorm"][ipix] * norm(map_struct["distmu"][ipix], map_struct["distsigma"][ipix]).pdf(r))**params["powerlaw_dist_exp"] / map_struct["pixarea"]
@@ -92,17 +155,28 @@ def get_catalog(params, map_struct):
     if params["doUseCatalog"]:
         map_struct['prob'] = prob 
 
-    cat['Sloc'] = Sloc
-    cat['S'] = S
+    idx = np.where(~np.isnan(S))[0]
+    ra, dec, Sloc, S = ra[idx], dec[idx], Sloc[idx], S[idx]
 
-    idx = np.argsort(Sloc)[::-1]
-    cat = cat[idx]
-    #top50 = cat[idx][:50]
-    # Take all objects with greater than 1% probability
     Sthresh = np.max(S)*0.01
-    cat = cat[cat['Sloc'] >= Sthresh]
+    idx = np.where(S >= Sthresh)[0]
+    ra, dec, Sloc, S = ra[idx], dec[idx], Sloc[idx], S[idx] 
+
+    idx = np.argsort(S)[::-1]
+    ra, dec, Sloc, S = ra[idx], dec[idx], Sloc[idx], S[idx] 
+
+    if len(ra) > 1000:
+        print('Cutting catalog to top 1000 galaxies...')
+        idx = np.arange(1000).astype(int)
+        ra, dec, Sloc, S = ra[idx], dec[idx], Sloc[idx], S[idx]
+
     catalogfile = os.path.join(params["outputDir"],'catalog.csv')
-    cat['RAJ2000', 'DEJ2000', 'Sloc', 'S'].write(catalogfile, format='csv', overwrite=True)
+    fid = open(catalogfile,'w')
+    cnt = 0
+    for a, b, c, d in zip(ra, dec, Sloc, S):
+        fid.write("%d %.5f %.5f %.5e %.5e\n"%(cnt,a,b,c,d))
+        cnt = cnt + 1
+    fid.close()
 
     return map_struct
 
--- gwemopt/tiles.py
+++ gwemopt/tiles.py
@@ -13,6 +13,37 @@ import gwemopt.utils
 import gwemopt.rankedTilesGenerator
 import gwemopt.samplers, gwemopt.segments
 import gwemopt.quadrants
+import gwemopt.moc
+
+def galaxy(params, map_struct):
+
+    nside = params["nside"]
+
+    catalogfile = os.path.join(params["outputDir"],'catalog.csv')
+    if not os.path.isfile(catalogfile):
+        print("Please enable --doCatalog")
+        exit(0)
+    cat = np.loadtxt(catalogfile)
+
+    tile_structs = {}
+    for telescope in params["telescopes"]:
+        config_struct = params["config"][telescope]
+
+        moc_struct = {}
+        for ii, tess in enumerate(cat):
+            index, ra, dec, Sloc, S = tess[0], tess[1], tess[2], tess[3], tess[4]
+            index = index.astype(int)
+            moc_struct[index] = gwemopt.moc.Fov2Moc(params, config_struct, telescope, ra, dec, nside)
+
+        tile_struct = powerlaw_tiles_struct(params, config_struct, telescope, map_struct, moc_struct)
+        tile_struct = gwemopt.segments.get_segments_tiles(config_struct, tile_struct)
+        for ii, tess in enumerate(cat):
+            index, ra, dec, Sloc, S = tess[0], tess[1], tess[2], tess[3], tess[4]
+            index = index.astype(int)
+            tile_struct[index]['prob'] = Sloc
+        tile_structs[telescope] = tile_struct
+
+    return tile_structs
 
 def greedy(params, map_struct):
 
