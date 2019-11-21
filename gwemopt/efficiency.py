9a2403cad8064bc8964513f621e3367711d48328
Michael Coughlin <coughlin@physics.harvard.edu>
2019-11-21 08:38:45 -0800

    fix efficiency

--- gwemopt/efficiency.py
+++ gwemopt/efficiency.py
@@ -3,6 +3,9 @@ import os, sys
 import numpy as np
 import healpy as hp
 from astropy.time import Time
+import scipy.stats
+from scipy.interpolate import interpolate as interp
+
 import matplotlib
 #matplotlib.rc('text', usetex=True)
 matplotlib.use('Agg')
@@ -15,6 +18,8 @@ import gwemopt.utils
 def compute_efficiency(params, map_struct, lightcurve_struct, coverage_struct):
 
     nside = params["nside"]
+    npix = hp.nside2npix(nside)
+
     Ninj = params["Ninj"]
     Ndet = params["Ndet"]
     gpstime = params["gpstime"]
@@ -22,41 +27,25 @@ def compute_efficiency(params, map_struct, lightcurve_struct, coverage_struct):
     #FOV_r = np.sqrt(float(params['FOV'])/np.pi)
 
     if params["doCatalog"]:
-        prob_data_sorted = np.sort(map_struct["prob_catalog"])[::-1]
-        prob_data_indexes = np.argsort(map_struct["prob_catalog"])[::-1]
+        distn = scipy.stats.rv_discrete(values=(np.arange(npix),
+                                                map_struct["prob_catalog"]))
     else:
-        prob_data_sorted = np.sort(map_struct["prob"])[::-1]
-        prob_data_indexes = np.argsort(map_struct["prob"])[::-1]
-    prob_data_cumsum = np.cumsum(prob_data_sorted)
-    rand_values = np.random.rand(Ninj,)
+        distn = scipy.stats.rv_discrete(values=(np.arange(npix),
+                                                map_struct["prob"]))
+    ipix = distn.rvs(size=Ninj)
+    ras, decs = hp.pix2ang(nside, ipix, lonlat=True)
+
     dists = np.logspace(-1,3,1000)
-    #dists = np.logspace(-1,np.log10(300),100)
     ndetections = np.zeros((len(dists),))
 
-    ras = []
-    decs = []
-
     for ii in range(Ninj):
-        # this returns the index of the value in cumsum of probablity closest to a random value
-        ipix = np.argmin(np.abs(prob_data_cumsum-rand_values[ii])) 
-        # the point corresponding to that
-        ra_inj = map_struct["ra"][prob_data_indexes][ipix]
-        dec_inj = map_struct["dec"][prob_data_indexes][ipix]
-
-        ras.append(ra_inj)
-        decs.append(dec_inj)
-
         # THE SKY-LOCATION AS A HEALPIXEL ID
-        pinpoint = hp.ang2pix(nside, theta=ra_inj, phi=dec_inj, lonlat=True)
-
-        #idxs = np.where(np.sqrt((coverage_struct["data"][:,0]-ra_inj)**2 + (coverage_struct["data"][:,1]-dec_inj)**2) <= FOV_r)[0]
-        #idxs_ra = np.where(np.abs(coverage_struct["data"][:,0]-ra_inj)<=coverage_struct["FOV"]/2.0)[0]
-        #idxs_dec = np.where(np.abs(coverage_struct["data"][:,1]-dec_inj)<=coverage_struct["FOV"]/2.0)[0]
-        #idxs = np.intersect1d(idxs_ra,idxs_dec)
+        pinpoint = hp.ang2pix(nside, theta=ras[ii], phi=decs[ii], lonlat=True)
 
         idxs = []
         for jj in range(len(coverage_struct["ipix"])):
             expPixels = coverage_struct["ipix"][jj]
+             
             if pinpoint in expPixels:
                 idxs.append(jj)
         if len(idxs) == 0:
@@ -71,23 +60,30 @@ def compute_efficiency(params, map_struct, lightcurve_struct, coverage_struct):
         for mjd, mag, filt in zip(mjds,mags,filts):
             lightcurve_t = lightcurve_struct["t"] + mjd_inj
             lightcurve_mag = lightcurve_struct[filt]
+            idx = np.where(np.isfinite(lightcurve_mag))[0]
 
-            lightcurve_mag_interp = np.interp(mjd,lightcurve_t,lightcurve_mag)
+            f = interp.interp1d(lightcurve_t[idx], lightcurve_mag[idx],
+                                fill_value='extrapolate')
+            lightcurve_mag_interp = f(mjd)
             dist_threshold = (10**(((mag-lightcurve_mag_interp)/5.0)+1.0))/1e6
 
             idxs_detections = np.where(dists <= dist_threshold)[0]
             detections[idxs_detections] = detections[idxs_detections] + 1
+
         idxs_detections = np.where(detections >= Ndet)[0]
         ndetections[idxs_detections] = ndetections[idxs_detections] + 1
+
     efficiency = ndetections / Ninj
     efficiency_struct = {}
-    efficiency_struct["ra"] = np.array(ras)
-    efficiency_struct["dec"] = np.array(decs)
+    efficiency_struct["ra"] = ras
+    efficiency_struct["dec"] = decs
     efficiency_struct["efficiency"] = efficiency
     efficiency_struct["distances"] = dists
+
     save_efficiency_data(params, efficiency_struct, lightcurve_struct)
     efficiency_metric = calculate_efficiency_metric(params, efficiency_struct)
     save_efficiency_metric(params, os.path.join(params["outputDir"],"efficiency.txt"), efficiency_metric, lightcurve_struct)
+
     return efficiency_struct
 
 
