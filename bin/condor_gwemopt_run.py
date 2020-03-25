e9835464390cf4803a739a6287300b7cf3b5cf19
Michael Coughlin <michael.coughlin@ligo.org>
2020-03-25 11:40:27 -0700

    update efficiency code

--- bin/condor_gwemopt_run.py
+++ bin/condor_gwemopt_run.py
@@ -19,13 +19,13 @@ configDirectory = "/home/michael.coughlin/gwemopt/config"
 
 events = ["S200105ae","S200213t","S200115j"]
 gpstimes = [1262276684.0572,1265602259.327981,1263122607.742047]
-skymaps = ["/home/michael.coughlin/gwemopt/data/S200105ae/LALInference.fits.gz","/home/michael.coughlin/gwemopt/data/S200213t/LALInference.fits.gz","/home/michael.coughlin/gwemopt/data/S200115j/LALInference.fits.gz"]
+skymaps = ["/home/michael.coughlin/gwemopt/data/S200105ae/LALInference.fits.gz","/home/michael.coughlin/gwemopt/data/S200213t/bayestar.fits.gz","/home/michael.coughlin/gwemopt/data/S200115j/LALInference.fits.gz"]
 
 lightcurveFiles = ["/home/michael.coughlin/gwemopt/lightcurves/Bulla_mejdyn0.005_mejwind0.110_phi45_45.6.dat","/home/michael.coughlin/gwemopt/lightcurves/Bulla_mejdyn0.005_mejwind0.010_phi45_45.6.dat","Tophat","Tophat"]
 modelTypes = ["file","file","Tophat","Tophat"]
 mags = [-16.0,-16.0,-16.0,-16.0]
 dmags = [0.0,0.0,0.0,0.5]
-exposuretimes = np.arange(60,3660,60)
+exposuretimes = np.arange(30,930,30)
 
 baseplotDir = "/home/michael.coughlin/gwemopt/output/efficiency"
 condorDir = "/home/michael.coughlin/gwemopt/condor"
@@ -37,6 +37,8 @@ if not os.path.isdir(logDir):
 job_number = 0
 
 fid = open(os.path.join(condorDir,'condor.dag'),'w')
+fid2 = open(os.path.join(condorDir,'condor.sh'),'w')
+
 for event,skymap,gpstime in zip(events,skymaps,gpstimes):
     for modelType,lightcurveFile,mag,dmag in zip(modelTypes,lightcurveFiles,mags,dmags):
         for exposuretime in exposuretimes:
@@ -45,21 +47,27 @@ for event,skymap,gpstime in zip(events,skymaps,gpstimes):
             else:
                 lcurve = lightcurveFile.split("/")[-1].replace(".dat","")
             outputDir = os.path.join(baseplotDir,event,lcurve,"%d"%(exposuretime))
+            #fid2.write('/home/michael.coughlin/gwemopt/bin/gwemopt_run --doRASlices --Tobs 0.0,3.0 --telescope ZTF --doSchedule --doSkymap --doPlots --skymap %s --gpstime %.2f --doTiles --doSingleExposure --filters g,r,g,r,g,r --powerlaw_cl 0.9 --doAlternatingFilters --doEfficiency --do3D --modelType %s --mag %.1f --dmag %.1f --lightcurveFiles %s -o %s --exposuretimes %d,%d --doUsePrimary -c %s\n' % (skymap,gpstime,modelType,mag,dmag,lightcurveFile,outputDir,exposuretime,exposuretime,configDirectory))
+
+            fid2.write('/home/michael.coughlin/gwemopt/bin/gwemopt_run --mindiff 10800 --Tobs 0.0,3.0 --telescope ZTF --doSchedule --doSkymap --doPlots --skymap %s --gpstime %.2f --doTiles --doSingleExposure --filters g,r,g,r,g,r --powerlaw_cl 0.9 --doEfficiency --do3D --modelType %s --mag %.1f --dmag %.1f --lightcurveFiles %s -o %s --exposuretimes %d,%d,%d,%d,%d,%d --doUsePrimary -c %s\n' % (skymap,gpstime,modelType,mag,dmag,lightcurveFile,outputDir,exposuretime,exposuretime,exposuretime,exposuretime,exposuretime,exposuretime,configDirectory))
+
             efficiency_file = os.path.join(outputDir,'efficiency.txt')
             if not os.path.isfile(efficiency_file):
                 fid.write('JOB %d condor.sub\n'%(job_number))
                 fid.write('RETRY %d 3\n'%(job_number))
-                #fid.write('VARS %d jobNumber="%d" skymap="%s" gpstime="%.2f" lightcurveFile="%s" outputDir="%s" modelType="%s" mag="%.1f" dmag="%.1f" exposuretimes="%d,%d"\n'%(job_number,job_number,skymap,gpstime,lightcurveFile,outputDir,modelType,mag,dmag,exposuretime,exposuretime))
-                fid.write('VARS %d jobNumber="%d" skymap="%s" gpstime="%.2f" lightcurveFile="%s" outputDir="%s" modelType="%s" mag="%.1f" dmag="%.1f" exposuretimes="%d"\n'%(job_number,job_number,skymap,gpstime,lightcurveFile,outputDir,modelType,mag,dmag,exposuretime))
+                fid.write('VARS %d jobNumber="%d" skymap="%s" gpstime="%.2f" lightcurveFile="%s" outputDir="%s" modelType="%s" mag="%.1f" dmag="%.1f" exposuretimes="%d,%d,%d,%d,%d,%d"\n'%(job_number,job_number,skymap,gpstime,lightcurveFile,outputDir,modelType,mag,dmag,exposuretime,exposuretime,exposuretime,exposuretime,exposuretime,exposuretime))
+                #fid.write('VARS %d jobNumber="%d" skymap="%s" gpstime="%.2f" lightcurveFile="%s" outputDir="%s" modelType="%s" mag="%.1f" dmag="%.1f" exposuretimes="%d"\n'%(job_number,job_number,skymap,gpstime,lightcurveFile,outputDir,modelType,mag,dmag,exposuretime))
                 fid.write('\n\n')
                 job_number = job_number + 1
 fid.close()
+fid2.close()
 
 fid = open(os.path.join(condorDir,'condor.sub'),'w')
 fid.write('executable = /home/michael.coughlin/gwemopt/bin/gwemopt_run\n')
 fid.write('output = logs/out.$(jobNumber)\n');
 fid.write('error = logs/err.$(jobNumber)\n');
-fid.write('arguments = --telescope ZTF --doSchedule --doSkymap --doPlots --skymap $(skymap) --gpstime $(gpstime) --doBalanceExposure --doTiles --doSingleExposure --filters g,r --powerlaw_cl 0.9 --doAlternatingFilters --doEfficiency --do3D --modelType $(modelType) --mag $(mag) --dmag $(dmag) --lightcurveFiles $(lightcurveFile) -o $(outputDir) --exposuretimes $(exposuretimes) --doUsePrimary -c %s\n' % (configDirectory)) 
+fid.write('arguments = --mindiff 10800 --Tobs 0.0,3.0 --telescope ZTF --doSchedule --doSkymap --doPlots --skymap $(skymap) --gpstime $(gpstime) --doTiles --doSingleExposure --filters g,r,g,r,g,r --powerlaw_cl 0.9 --doEfficiency --do3D --modelType $(modelType) --mag $(mag) --dmag $(dmag) --lightcurveFiles $(lightcurveFile) -o $(outputDir) --exposuretimes $(exposuretimes) --doUsePrimary -c %s\n' % (configDirectory))
+#fid.write('arguments = --doRASlices --telescope ZTF --doSchedule --doSkymap --doPlots --skymap $(skymap) --gpstime $(gpstime) --doTiles --doSingleExposure --filters g,r --powerlaw_cl 0.9 --doAlternatingFilters --doEfficiency --do3D --modelType $(modelType) --mag $(mag) --dmag $(dmag) --lightcurveFiles $(lightcurveFile) -o $(outputDir) --exposuretimes $(exposuretimes) --doUsePrimary -c %s\n' % (configDirectory)) 
 #fid.write('arguments = --telescope ZTF --doSchedule --doSkymap --doPlots --skymap $(skymap) --gpstime $(gpstime) --doTiles --doSingleExposure --filters g --powerlaw_cl 0.9 --doAlternatingFilters --doEfficiency --do3D --modelType $(modelType) --mag $(mag) --dmag $(dmag) --lightcurveFiles $(lightcurveFile) -o $(outputDir) --exposuretimes $(exposuretimes) --doUsePrimary -c %s\n' % (configDirectory)) 
 fid.write('requirements = OpSys == "LINUX"\n');
 fid.write('request_memory = 4000\n');
--- bin/efficiency_gwemopt_run.py
+++ bin/efficiency_gwemopt_run.py
@@ -15,6 +15,8 @@ plt.rcParams['xtick.labelsize']=24
 plt.rcParams['ytick.labelsize']=24
 from matplotlib.pyplot import cm
 
+from matplotlib.patches import Rectangle
+
 configDirectory = "/home/michael.coughlin/gwemopt/config"
 
 
@@ -23,13 +25,15 @@ gpstimes = [1262276684.0572,1265602259.327981,1263122607.742047]
 skymaps = ["/home/michael.coughlin/gwemopt/data/S200105ae/LALInference.fits.gz","/home/michael.coughlin/gwemopt/data/S200213t/LALInference.fits.gz","/home/michael.coughlin/gwemopt/data/S200115j/LALInference.fits.gz"]
 
 lightcurveFiles = ["/home/michael.coughlin/gwemopt/lightcurves/Bulla_mejdyn0.005_mejwind0.110_phi45_45.6.dat","/home/michael.coughlin/gwemopt/lightcurves/Bulla_mejdyn0.005_mejwind0.010_phi45_45.6.dat","Tophat","Tophat"]
-lightcurveLabels = ['KN: 0.005, 0.11', 'KN: 0.005, 0.01', 'Tophat: 16, 0',
-                    'Tophat: 16, 0.5']
+lightcurveLabels = ['KN: 0.005 $M_\odot$, 0.11 $M_\odot$', 
+                    'KN: 0.005 $M_\odot$, 0.01 $M_\odot$',
+                    'Tophat: -16 mag, 0 mag/day',
+                    'Tophat: -16 mag, 0.5 mag/day']
 
 modelTypes = ["file","file","Tophat","Tophat"]
 mags = [-16.0,-16.0,-16.0,-16.0]
 dmags = [0.0,0.0,0.0,0.5]
-exposuretimes = np.arange(60,3660,60)
+exposuretimes = np.arange(30,930,30)
 
 baseplotDir = "/home/michael.coughlin/gwemopt/output/efficiency"
 condorDir = "/home/michael.coughlin/gwemopt/condor"
@@ -48,6 +52,8 @@ for event,skymap,gpstime in zip(events,skymaps,gpstimes):
             if not lcurve in data[event]:
                 data[event][lcurve] = {}
                 data[event][lcurve]["label"] = lightcurveLabel
+                data[event][lcurve]["ntiles"] = []
+                data[event][lcurve]["probs"] = []
                 data[event][lcurve]["data"] = []
 
             efficiency_file = os.path.join(outputDir,'efficiency.txt')
@@ -55,15 +61,77 @@ for event,skymap,gpstime in zip(events,skymaps,gpstimes):
                 lines = [line.rstrip('\n') for line in open(efficiency_file,'r')]
                 line = lines[1].split("\t")
                 data[event][lcurve]["data"].append([exposuretime,float(line[4])])
+
+            schedule_file = os.path.join(outputDir,'coverage_ZTF.dat')
+            if os.path.isfile(schedule_file):
+                data_out = np.atleast_2d(np.loadtxt(schedule_file))
+                if data_out.size == 0: continue
+                data[event][lcurve]["ntiles"].append([exposuretime,len(data_out[:,0])])
+                data[event][lcurve]["probs"].append([exposuretime,np.sum(data_out[:,1])])
+        data[event][lcurve]["ntiles"] = np.array(data[event][lcurve]["ntiles"])
+        data[event][lcurve]["probs"] = np.array(data[event][lcurve]["probs"])
         data[event][lcurve]["data"] = np.array(data[event][lcurve]["data"])
+
+color2 = 'coral'
+color1 = 'cornflowerblue'
+color3 = 'darkgreen'
+color4 = 'darkmagenta'
+
+colors = [color1,color2,color3,color4]
+linestyles = ['-', '-.', ':','--']
 
 for event,skymap,gpstime in zip(events,skymaps,gpstimes):
     outputDir = os.path.join(baseplotDir,event)
     plt.figure(figsize=(10,6))
-    for lcurve in data[event].keys():
+    ax1 = plt.gca()
+    ax2 = ax1.twinx()
+    for ii, lcurve in enumerate(data[event].keys()):
         if data[event][lcurve]["data"].size == 0: continue
-        plt.plot(data[event][lcurve]["data"][:,0], data[event][lcurve]["data"][:,1], 'x', label=data[event][lcurve]["label"])
-    plt.legend(loc=3)
+        ax1.step(data[event][lcurve]["data"][:,0], data[event][lcurve]["data"][:,1], linestyles[ii], where='mid', label=data[event][lcurve]["label"], color=colors[ii])
+    ax1.legend(loc=9,ncol=2,labelspacing=0.3,columnspacing=0.3,handlelength=2,prop={'size':18})
+    for lcurve in data[event].keys():
+        ax2.step(data[event][lcurve]["ntiles"][:,0], data[event][lcurve]["probs"][:,1], '-', where='mid',label=data[event][lcurve]["label"], color='k')
+
+    ax1.set_xlabel('Exposure time [s]')
+    ax1.set_ylabel('Detection Efficiency')
+    ax2.set_ylabel('Integrated Probability')
+
+    ax1.plot([30,30],[0,1],'k--')
+    rect1 = Rectangle((120, 0), 180, 1, alpha=0.3, color='g')
+    ax1.add_patch(rect1)
+
+    plt.text(35, 0.55, "Survey")
+    plt.annotate('', xy=(30, 0.5), xytext=(110,0.5),
+                 arrowprops=dict(facecolor='black', arrowstyle='->'),
+                 )
+
+    plt.text(185, 0.53, "ToO")
+    plt.annotate('', xy=(120, 0.5), xytext=(300,0.5),
+                 arrowprops=dict(facecolor='black', arrowstyle='<->'),
+                 )
+
+    ax1.set_xlim([15,780])
+    ax1.set_ylim([0,1.0])
+    ax2.set_ylim([0,1.0])
+
     plotName = os.path.join(outputDir,'efficiency.pdf')
+    plt.savefig(plotName,bbox_inches='tight')
+    plt.close()
+
+    plt.figure(figsize=(10,6))
+    for lcurve in data[event].keys():
+        if data[event][lcurve]["ntiles"].size == 0: continue
+        plt.step(data[event][lcurve]["ntiles"][:,0], data[event][lcurve]["ntiles"][:,1], '.-', where='mid',label=data[event][lcurve]["label"])
+    plt.legend(loc=3)
+    plotName = os.path.join(outputDir,'ntiles.pdf')
+    plt.savefig(plotName,bbox_inches='tight')
+    plt.close()
+
+    plt.figure(figsize=(10,6))
+    for lcurve in data[event].keys():
+        if data[event][lcurve]["probs"].size == 0: continue
+        plt.step(data[event][lcurve]["probs"][:,0], data[event][lcurve]["probs"][:,1], 'x', label=data[event][lcurve]["label"])
+    plt.legend(loc=3)
+    plotName = os.path.join(outputDir,'probs.pdf')
     plt.savefig(plotName,bbox_inches='tight')
     plt.close()
