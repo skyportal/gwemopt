a962b149b54a08dca9f16327b951d8a3870a001d
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-04-15 13:01:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -111,10 +111,9 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Intended Audience :: Science/Research',
         'Intended Audience :: End Users/Desktop',
         'Intended Audience :: Developers',
