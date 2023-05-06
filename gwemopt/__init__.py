6ecd6e283799ea0efc064acbbc4de8aeb0172ebc
Robert Stein <rdstein@caltech.edu>
2023-05-05 17:45:45 -0700

    All in on pyproject.toml

--- gwemopt/__init__.py
+++ gwemopt/__init__.py
@@ -21,6 +21,5 @@
 """
 
 __author__ = 'Michael Coughlin <michael.coughlin@ligo.org>'
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
