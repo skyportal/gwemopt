3d8faa7674469850e4d33d88b42923b0f11db988
Robert Stein <rdstein@caltech.edu>
2023-05-09 22:40:56 -0700

    Update tests

--- gwemopt/tests/test_schedule.py
+++ gwemopt/tests/test_schedule.py
@@ -98,8 +98,7 @@ def test_scheduler():
             "coverage_ZTF.dat",
             "map.dat",
             "summary.dat",
-            "tiles_coverage_int.dat",
-        ]
+        ] + [f"tiles_coverage_int_{t}.txt" for t, _ in telescope_list]
 
         for extra_test_file in extra_test_files:
             new = pd.read_table(
