bf86dea57f24b5bd65bd1e49bdd2875685e2cf3d
Weizmann <kiend.weizman7@gmail.com>
2024-01-09 11:06:37 +0100

    Implement observability thresholds

--- gwemopt/run.py
+++ gwemopt/run.py
@@ -75,6 +75,20 @@ def run(args=None):
         if args.doPlots:
             print("Plotting observability...")
             plot_observability(params, map_struct)
+        
+        if args.doObservabilityExit:
+            for telescope in params["telescopes"]:
+                if np.sum(observability_struct[telescope]["prob"]) < args.observability_thresh:
+                    print('Observability for %s: %.5f < %.5f... exiting.' % (telescope, np.sum(observability_struct[telescope]["prob"]), args.observability_thresh))
+
+                    if params["doTrueLocation"]:
+                        lightcurve_structs = gwemopt.lightcurve.read_files(params, params["lightcurveFiles"])
+                        for key in lightcurve_structs.keys():
+                            filename = os.path.join(params["outputDir"],'efficiency_true_' + lightcurve_structs[key]['name'] + '.txt')
+                            fid = open(filename, 'w')
+                            fid.write('0')
+                            fid.close()
+                    exit(0)
 
     if params["splitType"] is not None:
         print("Splitting skymap...")
